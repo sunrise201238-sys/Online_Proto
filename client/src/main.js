2d4443591264093a386d939faf0a5f472adc5133
sunrise201238-sys <sunrise201238@gmail.com>
2026-05-14 22:28:52 +0900

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -109,7 +109,7 @@ const UNIT_DATA = {
     firePerMinute: 60,         // = 1000 ms cooldown (exact)
     spreadCount: 1,
     spreadAngle: 0.02,
-    damage: 35,
+    damage: 50,
     magCapacity: 5,
     reloadMs: 2500,
     autoReload: false,
