b21e67433ecfb9fa451575816d9a69d025135ab4
sunrise201238-sys <sunrise201238@gmail.com>
2026-05-22 02:28:22 +0900

    Add files via upload

--- client/src/main.js
+++ client/src/main.js
@@ -79,7 +79,7 @@ const UNIT_DATA = {
 
     // Weapon spec
     lockRange: 43,
-    projectileSpeed: 300,
+    projectileSpeed: 200,
     firePerMinute: 250,         // ≈ 697.67 ms cooldown
     spreadCount: 8,
     spreadAngle: THREE.MathUtils.degToRad(16),
