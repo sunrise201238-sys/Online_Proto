e952d841d5b9da91e202ee772d70d4a23f15c2fe
sunrise201238-sys <sunrise201238@gmail.com>
2026-06-20 12:01:32 +0900

    Add files via upload

--- client/src/main.js
+++ client/src/main.js
@@ -161,7 +161,7 @@ const UNIT_DATA = {
     // Pilot stats
     hp: 150,
     boostCap: 250,
-    walkSpeed: 16,
+    walkSpeed: 8,
     sprintSpeed: 11.76,
     boostDrain: 1.1,
     boostRegen: 4.59,
@@ -179,7 +179,8 @@ const UNIT_DATA = {
     damage: 4,
     magCapacity: 250,
     reloadMs: 5000,
-    autoReload: false
+    autoReload: false,
+    stun: { ms: 50, moveScale: 0.85 }   // light stun, same as the SMG
   }
 };
 
